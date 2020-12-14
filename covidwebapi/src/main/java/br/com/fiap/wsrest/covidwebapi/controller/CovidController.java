b6969bbcf4a13e796083cb7ca91de783cc5d018a
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-12-14 14:05:18 -0300

    Add Cors

--- covidwebapi/src/main/java/br/com/fiap/wsrest/covidwebapi/controller/CovidController.java
+++ covidwebapi/src/main/java/br/com/fiap/wsrest/covidwebapi/controller/CovidController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import br.com.fiap.wsrest.covidwebapi.service.CovidWebApiPaisesService;
  *
  */
 @RestController
+@CrossOrigin
 @RequestMapping("covid")
 public class CovidController {
 	
